b5879d967ed9f308d12a8e82dd4df4271387457b
Christoph Hoetzendorfer <christoph.hoetzendorfer@tractive.com>
2022-07-25 15:08:52 +0200

    Show stay duration in circle and correctly render most important element last

--- js/main.js
+++ js/main.js
@@ -7,14 +7,17 @@ var map = L
 L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-    maxZoom: 15
+    maxZoom: 17
     }).addTo(map);
 
 // Add a svg layer to the map
 L.svg().addTo(map);
 
+const svg = d3.select("#map").select("svg");
+
 d3.json("data/positions.json").then(data => {
     const markers = data
+    .sort((a, b) => a.minutesSpentInCluster - b.minutesSpentInCluster) //Sort to ensure the hotspot with the highest duration is drawn last 
     .map(item => {
         const firstPosition = item.positions[0];
         return {long: firstPosition.longitude, lat: firstPosition.latitude, stayDuration: item.minutesSpentInCluster };
@@ -28,30 +31,47 @@ function addMarkersToMap(markers) {
         .domain([0, d3.max(markers.map(marker => marker.stayDuration))])
         .range(["pink", "red"]);
 
-    // Select the svg area and add circles:
-    const circles = d3.select("#map")
-    .select("svg")
-    .selectAll("myCircles")
-    .data(markers)
-    .enter()
-    .append("circle")
-        .attr("cx", data => map.latLngToLayerPoint([data.lat, data.long]).x)
-        .attr("cy", data => map.latLngToLayerPoint([data.lat, data.long]).y)
-        .attr("r", 30)
-        .style("fill", data => colorScale(data.stayDuration))
-        .attr("stroke", "red")
-        .attr("stroke-width", 3)
-        .attr("fill-opacity", .7)
+    /* Define the data for the circles */
+    // The class must be added - see: https://stackoverflow.com/a/26576371
+    const circleGroupData = svg.selectAll("g.circle")
+        .data(markers);
+
+    /*Create and place the "blocks" containing the circle and the text */  
+    const circleGroup = circleGroupData.enter()
+        .append("g")
+        // The class must be added - see: https://stackoverflow.com/a/26576371
+        .attr("class", "circle")
+        .attr("transform", data => `translate(${map.latLngToLayerPoint([data.lat, data.long]).x}, ${map.latLngToLayerPoint([data.lat, data.long]).y})`);
+
+    /*Create the circle for each block */
+    circleGroup.append("circle")
+        .attr("r", getCircleRadius())
+        .attr("stroke","black")
+        .attr("fill", data => colorScale(data.stayDuration))
+        .attr("fill-opacity", .8);
+
+
+    /* Create the text for each block */
+    circleGroup.append("text")
+        .attr("x", 0)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .text(data => `${data.stayDuration} min`);
 }
 
 
 // Function that update circle position if something change
 function update() {
-    const currentZoomLevel = map._zoom;
+    d3.selectAll("g.circle")
+        .attr("transform", data => `translate(${map.latLngToLayerPoint([data.lat, data.long]).x}, ${map.latLngToLayerPoint([data.lat, data.long]).y})`);
+
     d3.selectAll("circle")
-        .attr("cx", data => map.latLngToLayerPoint([data.lat, data.long]).x)
-        .attr("cy", data => map.latLngToLayerPoint([data.lat, data.long]).y)
-        .attr("r", 4 * currentZoomLevel + 1);
+        .attr("r", getCircleRadius());
+}
+
+function getCircleRadius() {
+    const currentZoomLevel = map._zoom;
+    return 4 * currentZoomLevel + 1;
 }
 
 // If the user change the map (zoom or drag), I update circle position:
