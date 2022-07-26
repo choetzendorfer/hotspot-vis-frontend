28df4df87f2ecfb6cfdb62f8c65d60e750e8160e
Christoph Hoetzendorfer <christoph.hoetzendorfer@tractive.com>
2022-07-26 14:52:08 +0200

    Add first pie chart draft for time statistics

--- js/main.js
+++ js/main.js
@@ -20,21 +20,22 @@ d3.json("data/positions.json").then(data => {
     .sort((a, b) => a.minutesSpentInCluster - b.minutesSpentInCluster) //Sort to ensure the hotspot with the highest duration is drawn last 
     .map(item => {
         const firstPosition = item.positions[0];
-        return {long: firstPosition.longitude, lat: firstPosition.latitude, stayDuration: item.minutesSpentInCluster };
+        return {long: firstPosition.longitude, lat: firstPosition.latitude, stayDuration: item.minutesSpentInCluster, timeStatistics: item.timeStatistics };
     });
 
-    addMarkersToMap(markers);
+    //addMarkersToMap(markers);
+    createAndShowPieChart(markers);
 });
 
-function addMarkersToMap(markers) {
+function addMarkersToMap(data) {
     const colorScale = d3.scaleLinear()
-        .domain([0, d3.max(markers.map(marker => marker.stayDuration))])
+        .domain([0, d3.max(data.map(marker => marker.stayDuration))])
         .range(["pink", "red"]);
 
     /* Define the data for the circles */
     // The class must be added - see: https://stackoverflow.com/a/26576371
     const circleGroupData = svg.selectAll("g.circle")
-        .data(markers);
+        .data(data);
 
     /*Create and place the "blocks" containing the circle and the text */  
     const circleGroup = circleGroupData.enter()
@@ -61,9 +62,40 @@ function addMarkersToMap(markers) {
     showOrHideCircleLabels();
 }
 
+function createAndShowPieChart(data) {
+    const radius = getCircleRadius();
+    const colorScale = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
+
+    const arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius * 2);
+
+    const pie = d3.pie().value(data => data.stayDurationInMinutes);
+
+    const pies = svg.selectAll(".pie")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "pie")
+        .attr("transform", data => `translate(${map.latLngToLayerPoint([data.lat, data.long]).x}, ${map.latLngToLayerPoint([data.lat, data.long]).y})`);
+
+    pies.selectAll(".slice")
+        .data(data => pie(data.timeStatistics))
+        .enter()
+        .append("path")
+        .attr("class", "slice")
+        .attr("d", arc)
+        .style("fill", (d, i) => colorScale(d.data.stayDurationInMinutes));
+}
+
 
 // Function that update circle position if something change
 function update() {
+    updateCircleAppearance();
+    updatePieChartAppearance();
+}
+
+function updateCircleAppearance() {
     d3.selectAll("g.circle")
         .attr("transform", data => `translate(${map.latLngToLayerPoint([data.lat, data.long]).x}, ${map.latLngToLayerPoint([data.lat, data.long]).y})`);
 
@@ -71,6 +103,11 @@ function update() {
         .attr("r", getCircleRadius());
 
     showOrHideCircleLabels();
+}
+
+function updatePieChartAppearance() {
+    d3.selectAll(".pie")
+    .attr("transform", data => `translate(${map.latLngToLayerPoint([data.lat, data.long]).x}, ${map.latLngToLayerPoint([data.lat, data.long]).y})`);
 }
 
 function showOrHideCircleLabels() {
